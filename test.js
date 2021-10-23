086e2b96e6ca6770049f94c41a8aef94b90c0951
HackedZach <zay0106@icloud.com>
2021-10-23 14:43:55 -0400

    Merge

--- test.js
+++ test.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import SpotifyWebApi from 'spotify-web-api-node';
-
-const spotifyApi = new SpotifyWebApi();
-
-const clientId = '579af8ae35fb49b9b075531206b8f73d';
-const clientSecret = '965b1f8321f14da2a2ecd522ab74ab0e';
-
-const _getToken = async () => {
-
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded', 
-            'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-        },
-        body: 'grant_type=client_credentials'
-    });
-
-    const data = await result.json();
-    spotifyApi.setAccessToken(data.access_token); 
-    return data.access_token;
-}
-
-console.log(_getToken);
-=======
 import SpotifyWebApi from 'spotify-web-api-node'
 
 const spotifyApi = new SpotifyWebApi()
@@ -32,4 +6,3 @@ spotifyApi.setAccessToken(
 )
 
 spotifyApi.getTrack('4EWCNWgDS8707fNSZ1oaA5').then((data) => console.log(data))
->>>>>>> a313a2a602cc663dcc5d8c298e84e3808dccb927
