c4ac5790b5900aacd6ad301ed3b3e9247a9c5e7a
HackedZach <zay0106@icloud.com>
2021-10-26 17:02:45 -0400

    Revert "backend stuff"
    This reverts commit a313a2a602cc663dcc5d8c298e84e3808dccb927.

--- test.js
+++
@@ -1,8 +0,0 @@
-import SpotifyWebApi from 'spotify-web-api-node'
-
-const spotifyApi = new SpotifyWebApi()
-spotifyApi.setAccessToken(
-    'BQDJUngLYa7YSu_OFfvK6bU1S9EMGztqiSoUxOiw2yjRaDPtqMdDZ4Q8V7ZH7Dccm3_FW9kYxzk6RPPeO9Y'
-)
-
-spotifyApi.getTrack('4EWCNWgDS8707fNSZ1oaA5').then((data) => console.log(data))
